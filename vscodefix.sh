e32925e819b44a7a48e5c77d8d7a7a534d419c77
wahasa <69626847+wahasa@users.noreply.github.com>
2024-02-14 14:48:31 +0700

    Create vscodefix.sh

---
+++ vscodefix.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+case `uname -m` in
+      aarch64)
+           archurl="arm64" ;;
+      arm*)
+           archurl="armhf" ;;
+      x86_64)
+           archurl="amd64" ;;
+      *)
+           echo "unknown architecture"; exit 1 ;;
+      esac
+      wget "https://vscode.download.prss.microsoft.com/dbazure/download/stable/8b3775030ed1a69b13e4f4c628c612102e30a681/code_1.85.2-1705560689_${archurl}.deb"
+      wget "https://vscode.download.prss.microsoft.com/dbazure/download/stable/8b3775030ed1a69b13e4f4c628c612102e30a681/code_1.85.2-1705559800_${archurl}.deb"
+      wget "https://vscode.download.prss.microsoft.com/dbazure/download/stable/8b3775030ed1a69b13e4f4c628c612102e30a681/code_1.85.2-1705561292_${archurl}.deb"
+sleep 1
+sudo apt install ~/code*
+rm /usr/share/applications/code.desktop
+
+wget https://raw.githubusercontent.com/wahasa/Ubuntu/main/Patch/code.desktop -P /usr/share/applications/
+
+rm code*
+rm vscodefix.sh
