6e32722e39b6182b93d360d7bccc2d507f1b02e4
wahasa <69626847+wahasa@users.noreply.github.com>
2024-07-05 08:22:17 +0700

    parrot6.1.sh

--- Install/parrot6.1.sh
+++ Install/parrot6.1.sh
@@ -58,7 +58,7 @@ pulseaudio --start \
     --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" \
     --exit-idle-time=-1
 cd \$(dirname \$0)
-## unset LD_PRELOAD in case termux-exec is installed
+## Unset LD_PRELOAD in case termux-exec is installed
 unset LD_PRELOAD
 command="proot"
 command+=" --kill-on-exit"
@@ -75,9 +75,9 @@ command+=" -b /dev/null:/proc/sys/kernel/cap_last_cap"
 command+=" -b /proc"
 command+=" -b /data/data/com.termux/files/usr/tmp:/tmp"
 command+=" -b $folder/root:/dev/shm"
-#Uncomment the following line to have access to the home directory of termux
+## Uncomment the following line to have access to the home directory of termux
 #command+=" -b /data/data/com.termux/files/home:/root"
-#Uncomment the following line to mount /sdcard directly to /
+## Uncomment the following line to mount /sdcard directly to /
 command+=" -b /sdcard"
 command+=" -w /root"
 command+=" /usr/bin/env -i"
@@ -126,5 +126,5 @@ bash $linux
    #rm parrot6.1.sh
 
 #
-# Script edited by 'WaHaSa', Script V3-revision.
+## Script edited by 'WaHaSa', Script V3-revision.
 #
